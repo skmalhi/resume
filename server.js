c99820176b243d4111d5f8ae0700aa14cece8c93
skmalhi <skmalhi1996@gmail.com>
2021-03-03 12:08:59 +0500

    server modify

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-app.use(express.static(__dirname + "/resume"));
+app.use(express.static(__dirname + "/dist/resume"));
 
 app.get("/*", (req, res) => {
     res.sendFile(path.join(__dirname + "/dist/resume/index.html"));
