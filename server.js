714d4e711277aed72281ffb0ed5178e1d91a5157
skmalhi <skmalhi1996@gmail.com>
2021-03-03 12:29:08 +0500

    compression

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const compression = require("compression");
 const path = require("path");
 const app = express();
 
+app.use(compression());
 app.use(express.static(__dirname + "/dist/resume"));
 
 app.get("/*", (req, res) => {
