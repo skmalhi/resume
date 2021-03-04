2870648604c4b12119fdcf554d1abca751ae3202
skmalhi <skmalhi1996@gmail.com>
2021-03-04 13:38:28 +0500

    speed up

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { ContactService } from './services/contact.service';
+import { AppService } from './services/app.service';
 
 @NgModule({
   declarations: [],
   imports: [CommonModule, HttpClientModule],
   exports: [HttpClientModule],
-  providers: [ContactService],
+  providers: [ContactService, AppService],
 })
 export class CoreModule {}
